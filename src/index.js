2190541dca8f74aabec384ae48aa4e2d299ff952
Maud M <maud.ite.3@gmail.com>
2022-05-11 17:51:20 +0200

    correction2

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-import { createRoot } from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 
 import './style/index.css';
 
 import App from './App';
 
 
-const container = document.getElementById('root');
-
-const root = ReactDOM.createRoot(container);
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 
 
 root.render(
-  <App />
+  <App />,
 );
 
 
